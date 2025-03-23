9a965f01e5863769e998cdb7f5535dc178bd13e2
AzamatUzakov <azamatakmal053@gmail.com>
2025-03-23 15:17:19 +0500

    1.2

---
+++ src/components/custom/ProductDetails.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface ProductDetailsProps {
+
+}
+
+const ProductDetails: React.FC<ProductDetailsProps> = () => {
+    return (
+        <>
+        
+        </>
+
+    );
+}
+
+export default ProductDetails;
---
+++ src/components/custom/ProductList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface ProductListProps {
+    
+}
+ 
+const ProductList: React.FC<ProductListProps> = () => {
+    return (  
+        <>
+        
+        </>
+    );
+}
+ 
+export default ProductList;
